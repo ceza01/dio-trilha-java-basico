d0b24a9063326428f032e042da115c878e065e6f
Cesar Eduardo <123430152+ceza01@users.noreply.github.com>
2023-10-18 23:19:12 -0300

    adicionando estudo sobre Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,20 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+       //classe Operadores.java
+            double soma = 10.5 + 15.7;
+            int subtração = 113 - 25;
+            int multiplicacao = 20 * 7;
+            int divisao = 15 / 3;
+            int modulo = 18 % 3;
+            double resultado = (10 * 7) + (20/4);
+
+            // Quando desejamos verificar se uma variável é IGUAL A outra.
+            //!= Quando desejamos verificar se uma variável é DIFERENTE da outra.
+            //> Quando desejamos verificar se uma variável é MAIOR QUE a outra.
+            //>= Quando desejamos verificar se uma variável é MAIOR OU IGUAL a outra.
+            //< Quando desejamos verificar se uma variável é MENOR QUE outra.
+            //<= Quando desejamos verificar se uma variável é MENOR OU IGUAL a outra.
+      }
+   
+}
