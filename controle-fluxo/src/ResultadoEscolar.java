57cbee8fe02e81521bd96c8d58ac310d9b75ac10
Cesar Eduardo <123430152+ceza01@users.noreply.github.com>
2023-10-19 23:11:16 -0300

    adicionando pastas sobre estudos de condições

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,13 +1,11 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
+        /**O operador ternário substitui o if/else onde teriamos uma expressão if/then/else. 
+         * No exemplo abaixo o código nota >=7 seria o if, então, se nota > 7. O '?' é o then, então retorne "aprovado". 
+         * E o ':' é o else, senão retorna recuperação ou reprovado dependendo do da próxima condiçã. */
+        
         int nota = 6;
-       
-        if(nota >= 7)
-         System.out.println("Aprovado");
- 
-        else
-         System.out.println("Reprovado");
-     
-    }
-    
+		String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+		System.out.println(resultado);
+	}
 }
